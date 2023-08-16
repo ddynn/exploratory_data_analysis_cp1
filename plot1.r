e31c517115779b619fc3075360cc34c358c892cd
ddynn <47133866+ddynn@users.noreply.github.com>
2023-08-16 08:41:25 -0400

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,19 @@
+# title: 'Exploratory Data Analysis: Course Project 1'
+# author: "Yanan Dong"
+# date: '2023-08-16'
+
+# Plot 1
+
+library(dplyr)
+#Import and subset the power consumption data
+data <- read.table("household_power_consumption.txt",header = T,sep=";")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data<-data %>% filter(Date >= "2007-02-01" & Date <= "2007-02-02")
+
+# create plot
+hist(as.numeric(data$Global_active_power), main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")+ylimit = c(0,1200)
+
+# Save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
