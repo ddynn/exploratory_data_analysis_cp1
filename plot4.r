f947c2b483b78a5bdb65a7ec7b2058671d0135c8
ddynn <47133866+ddynn@users.noreply.github.com>
2023-08-28 10:52:51 -0400

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -1,37 +1,39 @@
-# title: 'Exploratory data Analysis: Course Project 1'
+# title: 'Exploratory Data Analysis: Course Project 1'
 # author: "Yanan Dong"
 # date: '2023-08-16'
 
-# Plot 4
-
-library(dplyr)
-
 #Import and subset the power consumption data
 data_raw <- read.table("household_power_consumption.txt",header = T,sep=";") %>% 
-  mutate(date=as.date(date, format = "%d/%m/%Y"))
+data <- data_raw[data_raw$Date %in% c("1/2/2007","2/2/2007") ,]
+  
+#str(data)
+data$data$datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+data$sm1 <- as.numeric(data$Sub_metering_1)
+data$sm2 <- as.numeric(data$Sub_metering_2)
+data$sm3 <- as.numeric(data$Sub_metering_3)
+data$gap <- as.numeric(data$Global_active_power)
+data$grp<- as.numeric(data$Global_reactive_power)
+data$voltage <- as.numeric(data$Voltage)
 
-data<-data_raw %>% filter(date >= "2007-02-01" & date <= "2007-02-02") %>% 
-  mutate(Global_active_power = as.numeric(Global_active_power),
-         datetime = as.POSIXct(paste(as.date(date), Time)))
 
-## Plot 4
-png(file="plot4.png", height=480, width=480)
+#Plot 4
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
 
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(data, {
-  plot(Global_active_power~datetime, type="l", 
-       ylab="Global Active Power", xlab="")
-  plot(Voltage~datetime, type="l", 
-       ylab="Voltage", xlab="")
-  plot(Sub_metering_1~datetime, type="l", 
-       ylab="Global Active Power  ", xlab="")
-  lines(Sub_metering_2~datetime,col='Red')
-  lines(Sub_metering_3~datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~datetime, type="l", 
-       ylab="Global Rective Power  ",xlab="")
-})
+#p4.1
+plot(data$datetime, data$gap, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+#p4.2
+plot(data$datetime, data$voltage, type="l", xlab="datetime", ylab="Voltage")
+
+#p4.3
+plot(data$datetime, data$sm1, type="l", ylab="Energy Submetering", xlab="")
+lines(data$datetime, data$sm2, type="l", col="red")
+lines(data$datetime, data$sm3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+#p4.4
+plot(data$datetime, data$grp, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 ## Saving to file
 dev.off()
