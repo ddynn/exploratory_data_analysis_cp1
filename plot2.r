897d499d3d28713f05b8550a257cd0dc706b21ea
ddynn <47133866+ddynn@users.noreply.github.com>
2023-08-28 10:32:59 -0400

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -2,20 +2,16 @@
 # author: "Yanan Dong"
 # date: '2023-08-16'
 
-# Plot 2
-
-library(dplyr)
-
 #Import and subset the power consumption data
-data_raw <- read.table("household_power_consumption.txt",header = T,sep=";") %>% 
-  mutate(Date=as.Date(Date, format = "%d/%m/%Y"))
+data_raw <- read.table("household_power_consumption.txt",header = T,sep=";") 
 
-data<-data_raw %>% filter(Date >= "2007-02-01" & Date <= "2007-02-02") %>% 
-  mutate(Global_active_power = as.numeric(Global_active_power),
-         datetime = as.POSIXct(paste(as.Date(Date), Time)))
+data<-data_raw[data_raw$Date %in% c("1/2/2007","2/2/2007") ,] 
+
+data$Global_active_power<-as.numeric(data$Global_active_power) 
+data$datetime<-strptime(paste(data$Date,data$Time, sep = " "), format="%d/%m/%Y %H:%M:%S")
 
 
 ## Plot 2
 png(file="plot2.png", height=480, width=480)
-plot(data$datetime,data$Global_active_power, xaxt = "n", type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+plot(data$datetime,data$Global_active_power, type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 dev.off()
