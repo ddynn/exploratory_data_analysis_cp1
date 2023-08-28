a2e58e2c6d43ecc17016eb0e9b278203f4450fd7
ddynn <47133866+ddynn@users.noreply.github.com>
2023-08-28 10:47:09 -0400

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -3,26 +3,21 @@
 # date: '2023-08-16'
 
 # Plot 3
-
-library(dplyr)
-
-#Import and subset the power consumption data
 data_raw <- read.table("household_power_consumption.txt",header = T,sep=";") %>% 
-  mutate(Date=as.Date(Date, format = "%d/%m/%Y"))
-
-data<-data_raw %>% filter(Date >= "2007-02-01" & Date <= "2007-02-02") %>% 
-  mutate(Global_active_power = as.numeric(Global_active_power),
-         datetime = as.POSIXct(paste(as.Date(Date), Time)))
+data <- data_raw[data_raw$Date %in% c("1/2/2007","2/2/2007") ,]
+  
+#str(data)
+data$datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+data$sm1 <- as.numeric(data$Sub_metering_1)
+data$sm2 <- as.numeric(data$Sub_metering_2)
+data$sm3 <- as.numeric(data$Sub_metering_3)
 
 ## Plot 3
 png(file="plot3.png", height=480, width=480)
 
-with(data, {
-  plot(Sub_metering_1~datetime, type="l",
-       ylab="Energy sub-metering", xlab="", xaxt = "n")
-  lines(Sub_metering_2~datetime,col='Red')
-  lines(Sub_metering_3~datetime,col='Blue')
-})
+plot(data$datetime, data$sm1, type="l", ylab="Energy Submetering", xlab="")
+lines(data$datetime, data$sm2, type="l", col="red")
+lines(data$datetime, data$sm3, type="l", col="blue")
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
